086b26277cad782865ffab99c7f4ba1e157031d5
Ralph Schaer <ralphschaer@gmail.com>
2016-09-29 19:00:35 +0200

    Upgrade to extjs 6.2.0

--- src/main/java/ch/rasc/eds/starter/config/DevelopmentConfig.java
+++ src/main/java/ch/rasc/eds/starter/config/DevelopmentConfig.java
@@ -15,23 +15,12 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import ch.ralscha.extdirectspring.util.ExtDirectSpringUtil;
 
 @Configuration
 @Profile("development")
-class DevelopmentConfig extends WebMvcConfigurerAdapter {
-
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		String userDir = System.getProperty("user.dir");
-		registry.addResourceHandler("/**")
-				.addResourceLocations(Paths.get(userDir, "client").toUri().toString())
-				.setCachePeriod(0);
-	}
+class DevelopmentConfig {
 
 	@Bean
 	public FilterRegistrationBean corsFilter() {
@@ -45,11 +34,6 @@ class DevelopmentConfig extends WebMvcConfigurerAdapter {
 		filter.setUrlPatterns(Collections.singleton("/*"));
 		filter.setOrder(SecurityProperties.DEFAULT_FILTER_ORDER - 1);
 		return filter;
-	}
-
-	@Override
-	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/").setViewName("forward:/index.html");
 	}
 
 	@EventListener
